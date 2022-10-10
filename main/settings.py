202aeee059a95e552eadb0d006083b5e3dc40613
FatihG34 <fthgnydn34@gmail.com>
2022-10-10 11:19:51 +0300

    user app added

--- main/settings.py
+++ main/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_extensions",
 
     # my apps 
     "blogApp",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -121,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = "uploads"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
